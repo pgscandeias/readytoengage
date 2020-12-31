0c244c9e9b527d060a764f4cb2f60d830278f7c1
Pedro de Carvalho <pgscandeias@gmail.com>
2020-12-31 18:46:34 +0100

    WIP: uuid4

--- src/engine/Ship.js
+++ src/engine/Ship.js
@@ -1,13 +1,20 @@
 export default class Ship {
+  name = ''
   type = ''
   model = ''
   guns = 0
   hp = 0
+  target = null
 
-  constructor (s) {
+  constructor (s = {}) {
+    this.name = s.name
     this.type = s.type
     this.model = s.model
     this.guns = s.guns
     this.hp = s.hp
+  }
+
+  static create () {
+    return new Ship()
   }
 }
---
+++ src/views/Duel.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="duel">
+    <div class="card player">
+      <p>Player</p>
+      <p>
+        {{ player.name }}
+      </p>
+    </div>
+
+    <div class="card mob">
+      <p>Mob</p>
+      <p>
+        {{ mob.name }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ships from '@/engine/data/ships'
+import { uuidv4 } from '@/engine/uuidv4'
+import Ship from '@/engine/Ship'
+
+export default {
+  name: 'Duel',
+  components: {
+  },
+  data () {
+    return {
+      player: {},
+      mob: {}
+    }
+  },
+  created () {
+    console.log(ships.transport.model)
+    console.log(uuidv4())
+
+    this.player = Ship.create()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.duel {
+  padding: 1rem;
+  display: inline-block;
+
+  .card {
+    display: block;
+    float: left;
+    padding: 0 1rem;
+    margin-right: 1rem;
+  }
+  .card:last-child {
+    margin-right: 0;
+  }
+}
+</style>
